eac6c47cdcc19d56742f9dda61523a30ba0e5f8f
ss.lee <seonsook.lee@twolinecode.com>
2022-11-04 17:29:50 +0900

    feat: add admin page #2

--- src/pages/admin/list.vue
+++ src/pages/admin/list.vue
@@ -2,10 +2,10 @@
   <div class="admin-list-wrapper">
     <div class="list-container q-mr-lg">
       <div class="toolbar-wrapper">
-        <q-space />
-        <q-input v-model="search" outlined dense type="search" class="q-mr-md">
+        <q-space/>
+        <q-input v-model="search" outlined dense type="search" class="q-mr-md" label="이메일 검색">
           <template v-slot:append>
-            <q-icon name="search"/>
+            <q-icon name="search" @click="onClickSearch"/>
           </template>
         </q-input>
       </div>
@@ -18,30 +18,44 @@
       />
     </div>
     <div class="detail-container">
-      <div class="title">회원 상세정보</div>
-      <div v-if="selectUser !== null " class="list-wrapper">
+      <div class="header-wrapper">
+        <div class="title">회원 상세정보</div>
+        <q-space/>
+        <template v-if="!isLoading">
+          <q-btn flat padding="xs" color="white" size="xs" icon="edit" class="q-mr-xs" @click="onClickEditMode"/>
+          <q-btn flat padding="xs" color="white" size="xs" icon="delete" @click="deleteUser" />
+        </template>
+      </div>
+      <div v-if="selectUser !== null" class="list-wrapper">
         <div v-if="isLoading" class="loading-wrapper">
           <q-spinner-ios
               color="primary"
               size="2em"
           />
         </div>
-        <div v-else>
+        <div v-else class="list-container">
           <div class="list-item">
             <div class="label">이름</div>
-            {{ selectUser.name }}
+            <q-input v-if="isEditMode" v-model="name" class="q-space" />
+            <div v-else>{{ selectUser.name }}</div>
           </div>
           <div class="list-item">
             <div class="label">이메일</div>
-            {{ selectUser.email }}
+            <div>{{ selectUser.email }}</div>
           </div>
           <div class="list-item">
             <div class="label">권한</div>
-            {{ selectUser.authority }}
+            <q-select v-if="isEditMode" v-model="auth" :options="authOptions" class="q-space" />
+            <div v-else>{{ selectUser.authority }}</div>
           </div>
           <div class="list-item">
             <div class="label">주소</div>
-            {{ selectUser.address.city }}, {{ selectUser.address.street }}, {{ selectUser.address.zipcode }}
+            {{ selectUser.address?.city }}, {{ selectUser.address?.street }}, {{ selectUser.address?.zipcode }}
+          </div>
+          <q-space />
+          <div class="button-wrapper">
+            <q-btn outline color="primary" unelevated label="취소" class="q-mr-sm" @click="editModeCancel"/>
+            <q-btn color="primary" label="수정완료" unelevated @click="onClickEdit" />
           </div>
         </div>
       </div>
@@ -65,6 +79,10 @@ const columns = [
   {name: 'authority', label: '권한', align: 'left', field: 'authority', sortable: false},
 ]
 const isLoading = ref(true)
+const name = ref()
+const auth = ref()
+const authOptions = ['ROLE_ADMIN', 'ROLE_USER']
+const isEditMode = ref(false)
 
 const fetchUserList = async () => {
   const data = {
@@ -94,6 +112,73 @@ const fetchUserDetail = async (email) => {
   }
 }
 
+const onClickSearch = async () => {
+  try {
+    const response = await axios.get(`https://jssampletest.herokuapp.com/api/member/?email=${search.value}`, headerInstance)
+    const data = response.data.data
+    if (data) {
+      search.value = ''
+      userList.value = [data]
+    } else {
+      search.value = ''
+      userList.value = []
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const deleteUser = async () => {
+  const email = selectUser.value?.email
+  await axios.delete(`https://jssampletest.herokuapp.com/api/member/${email}`, headerInstance)
+  await fetchUserList()
+}
+
+const onClickEditMode = () => {
+  isEditMode.value = true
+  name.value = selectUser.value?.name
+  // email.value = selectUser.value?.email
+  auth.value = selectUser.value?.authority
+}
+
+const editModeCancel = () => {
+  isEditMode.value = false
+}
+
+const editUser = async () => {
+  const data = {
+    name: name.value,
+    email: selectUser.value?.email
+  }
+
+  try {
+    const response = await axios.put('https://jssampletest.herokuapp.com/api/member/', data, headerInstance)
+    return response.status
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const editAuth = async () => {
+  const data = {
+    authority: auth.value,
+    email: selectUser.value?.email
+  }
+
+  try {
+    await axios.put('https://jssampletest.herokuapp.com/api/member/auth', data, headerInstance)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const onClickEdit = async () => {
+  await editUser()
+  await editAuth()
+  await fetchUserList()
+  selectUser.value = null
+}
+
 onMounted(() => {
   fetchUserList()
 })
@@ -109,28 +194,41 @@ onMounted(() => {
   }
 
   .detail-container {
-    min-height: 600px;
+    min-height: 500px;
     border: 1px solid var(--q-primary);
     border-radius: 4px;
 
-    .title {
+    .header-wrapper {
       display: flex;
       align-items: center;
-      font-size: 20px;
-      font-weight: 700;
       height: 60px;
       background: var(--q-primary);
-      color: white;
-      padding-left: 30px
+      padding: 0 20px;
+
+      .title {
+        font-size: 20px;
+        font-weight: 700;
+        color: white;
+      }
+
     }
 
     .list-wrapper {
       height: calc(100% - 60px);
+
       .loading-wrapper {
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100%;
+      }
+
+      .list-container {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        padding: 20px;
       }
     }
 
@@ -139,12 +237,16 @@ onMounted(() => {
       align-items: center;
       font-size: 16px;
       height: 60px;
-      padding-left: 30px;
 
       .label {
         width: 150px;
         font-weight: 700;
       }
+    }
+
+    .button-wrapper {
+      display: flex;
+      justify-content: flex-end;
     }
 
     .empty-description {
